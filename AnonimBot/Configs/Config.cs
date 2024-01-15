bf177430d400571dc8ff2cf7e32bbc38270460bf
sardoralgoritm <sardorsaminov5@gmail.com>
2024-01-15 12:15:35 +0500

    Console project o'chirildi

--- AnonimBot/Configs/Config.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AnonimBotWithAPI.Configs;
-
-public static class Config
-{
-    public const string TOKEN = "6037204335:AAGMNkdLZXSX_1hEfTvhaDlBVbHr3G9E7dE";
-    public const long ADMIN = 6200383764;
-}
